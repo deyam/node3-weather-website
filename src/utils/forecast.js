4bd93d181cdfe78750088a5c269fc547374fe3d2
Deya Motawie <deya@b789.local>
2020-08-16 04:10:13 +0200

    Add more info in the forecast data. Feels like and humidity were added and tested locally.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location: Code: '+ body.code + ' Error: ' + body.error, undefined)
         } else {
-          callback(undefined, body.current.weather_descriptions+'. It is currently '+body.current.temperature+' degrees out. There is a '+ body.current.precip + '% chance of rain.')
+          callback(undefined, body.current.weather_descriptions+'. It is currently '+body.current.temperature+' degrees out. It feels like '+ body.current.feelslike  +' degrees out. The humidity is ' + body.current.humidity + ' percent. There is a '+ body.current.precip + '% chance of rain.')
           // callback(undefined, body.daily.data[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
